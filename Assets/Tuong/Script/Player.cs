2b40395aef2ceb35be3706ee93d70b8b348b56bd
Tuongnee <tuongnqpk03750@gmail.com>
2024-06-26 14:43:06 +0700

    bổ sung animation player

---
+++ Assets/Tuong/Script/Player.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float runSpeed = 8f;
+    public float highJump = 8f;
+    private float left_Right;
+    private Rigidbody2D rb;
+    private Animator anim;
+    
+    private bool isFacingRight = true;
+    private bool grounded;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        left_Right = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2 (left_Right * runSpeed, rb.velocity.y);
+        flip();
+        if (Input.GetKey(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
+        anim.SetBool("run", left_Right != 0);
+        anim.SetBool("grounded" ,grounded);
+    }
+
+    void flip()
+    {
+        if((isFacingRight && left_Right < 0) || (!isFacingRight && left_Right > 0))
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 size = transform.localScale;
+            size.x *= -1;
+            transform.localScale = size;
+        }
+    }
+    
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, highJump);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "grounded")
+        {
+            grounded = true;
+        }
+    }
+}
