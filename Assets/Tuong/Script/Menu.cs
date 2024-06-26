f2d6df06ea778b91640bf31feee4866349f935d5
Tuongnee <tuongnqpk03750@gmail.com>
2024-06-26 09:01:29 +0700

    Bổ sung code

---
+++ Assets/Tuong/Script/Menu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject SettingPanel;
+
+    private void Start()
+    {
+        SettingPanel.SetActive(false);
+    }
+
+    public void OpenSetting()
+    {
+        SettingPanel.SetActive(true);
+    }
+
+    public void CloseSetting()
+    {
+        SettingPanel.SetActive(false);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void ChangeScene(int ID)
+    {
+        SceneManager.LoadScene(ID);
+    }
+}
