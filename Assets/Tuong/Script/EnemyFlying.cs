ed0c68e1e329fe2b6f367b8b4b5bdeede2d6d1f1
Tuongnee <tuongnqpk03750@gmail.com>
2024-06-28 23:50:12 +0700

    thay đổi cơ chế camera(bổ sung vùng chết), thêm quái đi theo người chơi, thêm cơ chế và sửa đổi lỗi debug

---
+++ Assets/Tuong/Script/EnemyFlying.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFlying : MonoBehaviour
+{
+    public float Speed;
+    private GameObject player;
+    public bool chase;
+    public Transform startingPoint;
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        if (player == null)
+            return;
+        if(chase == true)
+        {
+            Chase();
+        }
+        else
+        {
+            ReturnStartPoint();
+        }
+        Flip();o
+    }
+
+    private void ReturnStartPoint()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, startingPoint.position, Speed * Time.deltaTime);
+    }
+    private void Chase()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, Speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, player.transform.position) <= 5);
+    }
+
+    private void Flip()
+    {
+        if(transform.position.x > player.transform.position.x)
+        {
+            transform.rotation = Quaternion.Euler(0,0,0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+    }
+}
